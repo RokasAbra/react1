89c63565a0ee356a3d74904d9a7b9606032c14a9
RokasAbra <rokasabramavicius@gmail.com>
2022-05-23 11:35:14 +0300

    uzdv...

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 //1. Atvaizduoti masyvą dogs. Kiekvienas šuo atskirame kvadrate.
@@ -15,17 +16,42 @@ import './App.css';
 
 function App() {
     const dogs = ['šuo', 'šunius', 'Bobikas', 'kudlius', 'Šarikas', 'avigalvis'];
-    
+    const [dog, setDog] = useState([]);
+    dogs.sort((a, b) => a.length - b.length);
+    let lyg = 0;
+    let neLyg = 0;
+    // const vienasDog = () =>
+    // { for (let i = 0; i < dogs.length; i++) {
+    //   if (dogs % 2 == 0) {
+    //     dogs[i] = lyg
+    //     return lyg
+    //   }if (dogs % 2 !== 0) {
+    //     neLyg = i;
+    //   }return neLyg;
+       
+      
+    // }
+{
+    } 
+    console.log(lyg, neLyg);
+    console.log(dogs);
   return (
     <div className="App">
       <header className="App-header">
         <div className='kvc'>
+        {
+              dogs.map( (c, i) => <div className='kv' key={i}>{dogs[i]}</div>)
+            }
+
+            {
+              dogs.map((c, i ) => <div className='crc' key={c}>{dogs[i]} {i + 1}</div> )
+            }
+            
+            {/* <div className='kv'></div>
             <div className='kv'></div>
             <div className='kv'></div>
             <div className='kv'></div>
-            <div className='kv'></div>
-            <div className='kv'></div>
-            <div className='kv'></div>
+            <div className='kv'></div> */}
         </div>
       </header>
     </div>
