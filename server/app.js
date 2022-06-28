a8ace634edf94e864a55902d58295d035dcd9e51
RokasAbra <rokasabramavicius@gmail.com>
2022-06-28 12:09:04 +0300

    up

--- server/app.js
+++ server/app.js
@@ -40,7 +40,23 @@ app.get("/medziai", (req, res) => {
   });
 });
 
+app.get("/front/medziai", (req, res) => {
+  const sql = ` 
+    SELECT
+    t.title AS title, g.title AS good, height, type, t.id, GROUP_CONCAT(c.comm, '-^o^-') AS comms
+    FROM trees AS t
+    LEFT JOIN goods AS g
+    ON t.good_id = g.id
+    LEFT JOIN comments AS c
+    ON c.tree_id = t.id
+    GROUP BY t.id 
+  `;
+  con.query(sql, (err, result) => {
+    if (err) throw err;
 
+    res.send(result);
+  });
+});
 
 //create metodas yra post su url ir duomenimis
 app.post("/medziai", (req, res) => {
@@ -102,6 +118,21 @@ app.post("/gerybes", (req, res) => {
   });
 });
 
+
+app.post("/front/komentarai", (req, res) => {
+  const sql = ` 
+    INSERT INTO comments
+    (comm, tree_id)
+    VALUES (?, ?) 
+    
+  `; /// klaustukai atitinka kintamuosius
+
+  con.query(sql, [req.body.com, req.body.treeId], (err, result) => {
+    if (err) throw err;
+
+    res.send({result, msg: {text: 'OK, Zuiki', type: 'success'}});
+  });
+});
 // Read metodas get
 app.get("/gerybes", (req, res) => {
   const sql = ` 
@@ -111,7 +142,25 @@ app.get("/gerybes", (req, res) => {
     RIGHT JOIN goods AS g
     ON t.good_id = g.id
     Group BY g.id
-    ORDER BY g.title  
+    ORDER BY g.title 
+  `;
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+
+    res.send(result);
+  });
+});
+
+///Front gerybes 
+app.get("/front/gerybes", (req, res) => {
+  const sql = ` 
+    SELECT
+    g.title, g.id, COUNT(t.id) AS trees_count, GROUP_CONCAT(t.title) as trees_title
+    FROM trees AS t
+    RIGHT JOIN goods AS g
+    ON t.good_id = g.id
+    Group BY g.id
+    ORDER BY g.title 
   `;
   con.query(sql, (err, result) => {
     if (err) throw err;
